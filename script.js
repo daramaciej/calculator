2ba4578d75ce3ba41d573c27151973cd9c099115
Maciej Dara <daramaciej@gmail.com>
2022-07-21 11:58:04 +0200

    Add delete button function

--- script.js
+++ script.js
@@ -1,60 +1,100 @@
 let firstOperand = ''
 let currentOperand = ''
 let currentOperation = null
+let result = 0
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]');
 const display = document.getElementById("display")
 const clearButton = document.querySelector('[data-clear]')
 const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
 
 function appendNumber(number) {
-    display.textContent += number
-    currentOperand = display.textContent
+    if (number === '.' && currentOperand.includes('.')) return
+    currentOperand = currentOperand.toString() + number.toString()
+}
+
+function updateDisplay () {
+    display.textContent = currentOperand
 }
 
 function clearDisplay() {
     display.textContent = ''
 }
 
-function add(a, b) {
-    return a + b
+function deleteNumber () {
+    currentOperand = currentOperand.slice(0, currentOperand.length -1)
+    updateDisplay()
+}
+
+function clear() {
+    clearDisplay()
+    firstOperand = ''
+    currentOperand = ''
+    currentOperation = null
 }
 
 function setOperation(operation) {
-    if(currentOperation !== null) operate()
-    firstOperand = display.textContent
+    if (currentOperand === '') {
+        currentOperation = operation
+        return
+    }
+    if (firstOperand !== null) {
+        operate() 
+    }
     currentOperation = operation
-    clearDisplay()
+    firstOperand = currentOperand
+    currentOperand = ''
 }
 
 
 function operate() {    
     a = Number(firstOperand)
     b = Number(currentOperand)
-    result = ''
-
-    console.log(currentOperation)
-    console.log(firstOperand)
-    console.log(currentOperand)
+    if (isNaN(a) || isNaN(b)) return
     switch(currentOperation) {
         case '+':
-            result = add(a, b)
-    }
-    display.innerHTML = result
+            result = a + b
+            break
+        case '-':
+            result = a - b
+            break
+        case '*':
+            result = a * b
+            break
+        case '/':
+            result = a / b
+            break
+        default:
+            return
+    }   
+    currentOperand = Math.round(result * 100) / 100
+    firstOperand = ''
+    currentOperation = undefined
+    updateDisplay()
 }
 
 numberButtons.forEach(button => 
-    button.addEventListener('click', () => appendNumber(button.textContent))
+    button.addEventListener('click', () => {
+        appendNumber(button.textContent)
+        updateDisplay()
+    })
 )
 
 operationButtons.forEach(button =>
-    button.addEventListener('click', () => setOperation(button.textContent))
+    button.addEventListener('click', () => {
+        setOperation(button.textContent)
+    })
 )
 
-clearButton.addEventListener('click', () => clearDisplay())
+clearButton.addEventListener('click', () => clear())
+
+deleteButton.addEventListener('click', () => deleteNumber())
 
 equalsButton.addEventListener('click', () => operate())
+
+
 
 /**
  * - wybieramy 2
